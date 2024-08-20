5690908b3339abce6fc4fa0bc0facb838e0259cd
Pvboni <162942016+Pvboni@users.noreply.github.com>
2024-08-19 21:15:08 -0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,7 +1,22 @@
-import pywhatkit as kit
+import os
+from twilio.rest import Client
 
-def enviar_mensagem():
-    # Substitua 'SEU_NUMERO' pelo número de telefone com o código do país, por exemplo, '+5511999999999'
-    kit.sendwhatmsg("+5511998995650", "Lembrete: Jantar no domingo!", 12, 0)
+# Retrieve Twilio Account SID and Auth Token from environment variables
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
-enviar_mensagem()
+# Verify if account_sid and auth_token are retrieved successfully
+if account_sid is None or auth_token is None:
+    print("Twilio credentials not found. Please set the environment variables TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN.")
+    exit(1)
+
+client = Client(account_sid, auth_token)
+
+# Send a message
+message = client.messages.create(
+    body='join skin-sad',
+    from_='whatsapp:+14155238886',  # Twilio sandbox number
+    to='whatsapp:+5511998995650'  # Your WhatsApp number
+)
+
+print(message.sid)
