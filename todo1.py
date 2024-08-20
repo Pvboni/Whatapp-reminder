804e1bcf225f022b2ccb911e3a57ff002ebbde92
Pvboni <162942016+Pvboni@users.noreply.github.com>
2024-08-20 14:03:46 -0300

    Update todo1.py

--- todo1.py
+++ todo1.py
@@ -1,6 +1,7 @@
 import csv
 import os
-import datetime
+import base64
+import requests
 from todoist_api_python.api import TodoistAPI
 from dotenv import load_dotenv
 
@@ -13,37 +14,43 @@ if not api_key:
     print("API key not found.")
     exit(1)
 
+# Repositório e caminho do arquivo no GitHub
+repo = "Pvboni/Whatapp-reminder"
+path = "tarefas1.csv"
+
 api = TodoistAPI(api_key)
 
 try:
-    # Data atual
-    hoje = datetime.datetime.now().strftime('%Y-%m-%d')
-
-    # Obter todas as tarefas
-    tasks = api.get_tasks()
+    # Obter todas as tarefas abertas
+    tasks = api.get_tasks(filter="(today | overdue)")
 
     # Verificar se há tarefas
     if not tasks:
         print("Nenhuma tarefa encontrada.")
         exit(0)
 
+    # Ordenar as tarefas pela data de conclusão
+    tasks.sort(key=lambda x: x.due.date if x.due else '')
+
     # Definir o caminho do arquivo CSV
+    directory = os.getcwd()
     file_name = 'tarefas1.csv'
-    file_path = os.path.join(os.getcwd(), file_name)
+    file_path = os.path.join(directory, file_name)
 
     # Criar ou abrir o arquivo CSV
     with open(file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Tarefa', 'Dia', 'Hora'])
 
+        # Escrever as tarefas no CSV
         for task in tasks:
             due_date = task.due.date if task.due else 'Sem data'
             due_time = task.due.datetime if task.due and task.due.datetime else 'Sem hora'
+            writer.writerow([task.content, due_date, due_time])
+            print(f"Tarefa adicionada: {task.content} - {due_date} {due_time}")
 
-            if due_date == hoje:
-                writer.writerow([task.content, due_date, due_time])
-
-    print(f"Arquivo CSV gerado com sucesso em: {file_path}")
+    print("Arquivo CSV gerado com sucesso.")
+    print(f"Arquivo CSV salvo em: {os.path.abspath(file_path)}")
 
 except Exception as e:
     print(f"Erro ao gerar o arquivo CSV: {e}")
