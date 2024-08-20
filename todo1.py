3857cd36721d333cc2e7d58f1fc0ce4f00089413
Pvboni <162942016+Pvboni@users.noreply.github.com>
2024-08-20 14:05:57 -0300

    Update todo1.py

--- todo1.py
+++ todo1.py
@@ -1,56 +1,38 @@
-import csv
-import os
-import base64
-import requests
-from todoist_api_python.api import TodoistAPI
-from dotenv import load_dotenv
+name: Generate CSV from Todoist
 
-# Carregar variáveis de ambiente
-load_dotenv()
+on:
+  workflow_dispatch:  # Manually trigger the workflow
 
-# Chave da API do Todoist
-api_key = os.getenv('YOUR_TODOIST_API_KEY')
-if not api_key:
-    print("API key not found.")
-    exit(1)
+jobs:
+  run_python_script:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v2
 
-# Repositório e caminho do arquivo no GitHub
-repo = "Pvboni/Whatapp-reminder"
-path = "tarefas1.csv"
+    - name: Set up Python
+      uses: actions/setup-python@v2
+      with:
+        python-version: '3.9'
 
-api = TodoistAPI(api_key)
+    - name: Install dependencies
+      run: |
+        pip install python-dotenv todoist-api-python requests
 
-try:
-    # Obter todas as tarefas abertas
-    tasks = api.get_tasks(filter="(today | overdue)")
+    - name: Run Python script
+      env:
+        YOUR_TODOIST_API_KEY: ${{ secrets.YOUR_TODOIST_API_KEY }}
+      run: python todo1.py
 
-    # Verificar se há tarefas
-    if not tasks:
-        print("Nenhuma tarefa encontrada.")
-        exit(0)
+    - name: Display CSV file content
+      run: cat tarefas1.csv
 
-    # Ordenar as tarefas pela data de conclusão
-    tasks.sort(key=lambda x: x.due.date if x.due else '')
-
-    # Definir o caminho do arquivo CSV
-    directory = os.getcwd()
-    file_name = 'tarefas1.csv'
-    file_path = os.path.join(directory, file_name)
-
-    # Criar ou abrir o arquivo CSV
-    with open(file_path, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Tarefa', 'Dia', 'Hora'])
-
-        # Escrever as tarefas no CSV
-        for task in tasks:
-            due_date = task.due.date if task.due else 'Sem data'
-            due_time = task.due.datetime if task.due and task.due.datetime else 'Sem hora'
-            writer.writerow([task.content, due_date, due_time])
-            print(f"Tarefa adicionada: {task.content} - {due_date} {due_time}")
-
-    print("Arquivo CSV gerado com sucesso.")
-    print(f"Arquivo CSV salvo em: {os.path.abspath(file_path)}")
-
-except Exception as e:
-    print(f"Erro ao gerar o arquivo CSV: {e}")
+    - name: Commit and push changes
+      run: |
+        git config --global user.name 'GitHub Action'
+        git config --global user.email 'action@github.com'
+        git add -A
+        git commit -m "Atualizando arquivo CSV de tarefas" || echo "No changes to commit"
+        git push
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
